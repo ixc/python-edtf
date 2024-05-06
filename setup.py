a7091f6446baa0fc7c2ca30755229e8b906d0f2c
Cole Crawford <16374762+ColeDCrawford@users.noreply.github.com>
2024-05-06 16:45:21 -0400

    WIP moving CI to Github Actions
    - Tests are failing due to testInterval(), but are being run correctly. On Python 3.12, `nose` is failing. We should upgrade to `nose2`, or more likely, switch from unittest + nose to pytest.
    - Move tox config to setup.cfg for now
    - TODO move to pyproject.toml instead of setup.cfg
    - TODO remove .travis.yml

--- setup.py
+++ setup.py
@@ -48,7 +48,10 @@ setuptools.setup(
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
 )
