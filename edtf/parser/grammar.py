df980aad1ebab0a7563699dae5bdedf0781fa574
rettinghaus <klaus.rettinghaus@gmail.com>
2026-01-02 19:04:50 +0100

    fix formatting with ruff

--- edtf/parser/grammar.py
+++ edtf/parser/grammar.py
@@ -322,7 +322,9 @@ MultipleDates.set_parser(inclusiveList)
 
 
 # (* *** L2 Season *** *)
-seasonL2Number = one_of("21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41")
+seasonL2Number = one_of(
+    "21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41"
+)
 l2season = year + "-" + seasonL2Number("season")
 Level2Season.set_parser(l2season)
 
